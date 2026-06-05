296781ce1119bd3069eaa20ee03905faf37327ed
William McKinnon <contact@willmckinnon.com>
2026-06-05 01:35:18 -0400

    remove delay, add pop in / out

--- include/sway/animation_manager.h
+++ include/sway/animation_manager.h
@@ -16,7 +16,6 @@ struct animation {
 	struct sway_container *con;
 	float multiplier;
 	bool initialized;
-	int delay_ms;
 	void (*update)(struct sway_container *);
 	void (*complete)(struct sway_container *);
 };
@@ -28,11 +27,9 @@ struct animation init_animation(struct sway_container *con);
 void refresh_animation_manager_timing();
 
 void add_animation(struct animation *animation, void (*update_callback)(struct sway_container *),
-	void (*complete_callback)(struct sway_container *), int delay_ms);
+	void (*complete_callback)(struct sway_container *));
 
 void finish_animation(struct animation *animation);
-
-void delay_other_animations(struct sway_container *con, int delay_ms);
 
 void start_animations();
 
